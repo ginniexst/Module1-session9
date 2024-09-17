002427363729f25bf7cb33783ed7c925ca3ece5b
Ginnie <ginniexst@gmail.com>
2024-09-17 22:25:45 +0900

    Add files via upload

---
+++ btvn.js
@@ -0,0 +1,91 @@
+// Ex1
+
+// let birthYear = +prompt("Bạn sinh năm bao nhiêu");
+// if (birthYear > 1900 && birthYear < 2024) {
+//     let age = 2024 - birthYear;
+//     console.log(age);
+// }
+// else {
+//     console.log("Năm sinh không hợp lệ.")
+// }
+
+//Ex2
+
+// let n = +prompt("Vui lòng nhập vào một số");
+// let divRemainer = n%2;
+// if (divRemainer === 1) {
+//     console.log("Số nhập vào là số lẻ");
+// }
+// else if (divRemainer === 0) {
+//     console.log("Sô nhập vào là sỗ chẵn");
+// }
+// else {
+//     console.log("Số nhập vào không hợp lệ");
+// }
+
+//Ex3
+
+// switch (+prompt("Nhập vào một số bất kỳ từ 2 đến 8")) {
+//     case 2:
+//         console.log("Monday");
+//         break;
+//     case 3:
+//         console.log("Tuesday");
+//         break;
+//     case 4:
+//         console.log("Wednesday");
+//         break;
+//     case 5:
+//         console.log("Thursday");
+//         break;
+//     case 6:
+//         console.log("Friday");
+//         break;
+//     case 7:
+//         console.log("Saturday");
+//         break;
+//     case 8:
+//         console.log("Sunday");
+//         break;
+// }
+
+// Ex4
+
+// let n = +prompt("Nhập vào một số nguyên");
+// (n % 2 === 0) ? console.log("Số nhập vào là số chẵn") : console.log("Số nhập vào là số lẻ")
+
+// Ex5
+
+// let n = +prompt("Nhập vào một số nguyên dương");
+// for (let i = 1; i <= n; i = i + 1) {
+//     console.log(i);
+// }
+
+// Ex6
+
+// let n = +prompt("Nhập vào một số nguyên dương");
+// let factors = 1;
+// for (i = 1; i <= n; i = i + 1) {
+//     factors = factors * i;
+// }
+// console.log(factors);
+
+// Ex7
+
+// for (i = 1; i <= 100; i = i + 1) {
+//     if (i % 15 === 0) {
+//         console.log("FizzBuzz");
+//     }
+//     else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     }
+//     else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+// Ex8
+
